6729fa2a18a688f27b4fb9dc42c52978c9bd60a9
zhaorong <1847195827@qq.com>
2021-06-17 17:39:35 +0800

    登录功能的实现

--- src/main.js
+++ src/main.js
@@ -1,11 +1,16 @@
 import Vue from 'vue'
-import App from './App.vue'
+import App from './App.vue'   //App根组件
 import router from './router'
 import './plugins/element.js'
+import './assets/CSS/global.css'
+import axios from 'axios'   //引入axios
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+//'http://127.0.0.1:8888/api/private/v1/'  这个地址是自己电脑本地的后台
+Vue.prototype.$http = axios    //把axios挂载到vue上
 
 Vue.config.productionTip = false
 
 new Vue({
-  router,
-  render: h => h(App)
+  router,     //将路由router挂载在实例上
+  render: h => h(App)   //通过render函数将App根组件渲染到页面上
 }).$mount('#app')
--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,16 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../components/Login.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    // name: 'Home',
+    // component: Home
+    redirect: '/login'
   },
   {
     path: '/about',
@@ -17,11 +19,33 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    component: Login
+  },
+  {
+    path: '/home',
+    component: Home
   }
 ]
 
 const router = new VueRouter({
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  //to 将要访问的路径
+  //from 代表从哪个路径跳转而来
+  //next 是一个函数，表示放行
+  //next() 放行    next('/login') 强制跳转
+
+  if (to.path === '/login') return next()   //如果访问路径是登录页面，则直接放行
+  //如果不是，则获取token，如果存在则转去想要去的页面，如果不存在则强制转去登录页面
+  const tokenStr = window.sessionStorage.getItem('token');
+  if (!tokenStr) return next('/login')
+  next()
+
 })
 
 export default router
